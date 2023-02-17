f84c2680fec8243d18720e5b2b327fe2fd0deb86
sayedazp <sayedazp@gmail.com>
2023-02-16 17:39:47 -0800

    task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str1[] = "with proper grammar, but the outcome is a piece of art,";
+	printf("%s\n", str1);
+	return (0);
+}
