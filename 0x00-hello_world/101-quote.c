471580e6f13b6a6d7b3959c9fd071b76fd3fcc6b
sayedazp <sayedazp@gmail.com>
2023-02-16 18:17:52 -0800

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 5, 1, stderr);
-	return (1);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59, 1, stderr);
+    return (1);
 }
