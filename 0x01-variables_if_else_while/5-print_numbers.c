d7710125ca7d3d6123151d36ecd62498018e80b2
sayedazp <sayedazp@gmail.com>
2023-02-17 07:37:18 -0800

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point func
+ * Return: always zero
+*/
+int main(void)
+{
+	for (char n = 0; n<10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
