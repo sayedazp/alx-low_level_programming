dec63e2f89d30d09f7f9189c7b6b56a653f623c9
sayedazp <sayedazp@gmail.com>
2023-02-18 05:06:08 -0800

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - the entry point function
+ * Return: always 0
+*/
+int main(void)
+{
+	char c = 'a';
+
+	while (c != 'z' + 1)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
