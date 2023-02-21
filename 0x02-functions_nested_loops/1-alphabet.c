d853b261e4ba4e7e6ef2a4743f753501b764d0f9
sayedazp <sayedazp@gmail.com>
2023-02-21 12:04:45 -0800

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,12 @@
 #include "main.h"
+/**
+ * print_alphabet - printing alphabet from a to z
+ * Return: no return it's a void
+ */
 void print_alphabet(void)
 {
 	char c = 'a';
+
 	while (c != 'z' + 1)
 	{
 		_putchar(c);
