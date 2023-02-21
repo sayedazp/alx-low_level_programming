cb3ab4387593711dfdafa3ed446a5d791452137f
sayedazp <sayedazp@gmail.com>
2023-02-21 12:11:40 -0800

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - printing alphabet 10 times
+ * Return: no return 
+*/
+void print_alphabet_x10(void)
+{
+	int n = 0;
+	while (n <= 9)
+	{
+		print_alphabet();
+	}
+}
