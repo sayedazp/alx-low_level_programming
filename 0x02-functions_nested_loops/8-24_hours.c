e70e25a5a78f99b67487f5d7c8ed1dcb4ed2329b
sayedazp <sayedazp@gmail.com>
2023-02-21 15:37:41 -0800

    task8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	a = 0;
+	
+	while (a <= 2)
+	{
+		b = 0;
+		
+		while ((!(b > 3 && a == 2)) && (b <= 9))
+		{
+			c = 0;
+
+			while (c <= 5)
+			{
+				d = 0;
+
+				while (d <= 9)
+				{
+					_putchar('0' + a);
+					_putchar('0' + b);
+					_putchar(':');
+					_putchar('0' + c);
+					_putchar('0' + d);
+					d++;
+				}
+				c++;
+			}
+			b++;
+
+		}
+		a++;
+
+	}
+
+}
