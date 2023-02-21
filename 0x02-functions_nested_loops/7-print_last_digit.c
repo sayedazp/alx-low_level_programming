d2961fef6f30c3508239d2fa625218b2654e62bd
sayedazp <sayedazp@gmail.com>
2023-02-21 14:54:51 -0800

    task7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,15 +2,15 @@
 /**
  * print_last_digit - a function to return last digit
  * @n: the checked variable
- * return: the last digit
+ * Return: the last digit
 */
 
 int print_last_digit(int n)
 {
 	int l = n % 10;
 
-       if (l < 0)
-		l *= -1;	       
+	if (l < 0)
+		l *= -1;
 	_putchar('0' + l);
 	return (l);
 }
