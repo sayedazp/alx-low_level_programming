8c9d7e53aee71d7dda0ce901bcf060ea487fb018
sayedazp <sayedazp@gmail.com>
2023-02-21 14:26:40 -0800

    task

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,9 @@
+#include "main.h"
 /**
  * print_sign - a function prints the sign of the input
  * @n: the checked variable
  * Return: 0 or 1 or -1 determining the state of the input
 */
-
 int print_sign(int n)
 {
 	if (n > 0)
