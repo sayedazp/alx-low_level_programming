ceab29e1db7e1085579b741a51eb15909c4fd026
sayedazp <sayedazp@gmail.com>
2023-02-21 19:00:10 -0800

    task16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,7 +6,7 @@
 
 int main(void)
 {
-	long unsigned prev = 1, prevv = 0, c = 0, current;
+	unsigned long prev = 1, prevv = 0, c = 0, current;
 
 	while (c  < 97)
 	{
@@ -14,7 +14,7 @@ int main(void)
 		prevv = prev;
 		prev = current;
 
-		if (c < 95)
+		if (c < 96)
 			printf("%lu, ", current);
 		else
 			printf("%lu", current);
