5b83559720b4af87aa769864f1f1e49ded627fbc
sayedazp <sayedazp@gmail.com>
2023-02-21 16:21:55 -0800

    task9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+int main(void)
+{
+	int a = 0, b = 0, prod;
+	
+	while (a <= 9)
+	{
+		b = 0;
+
+		while (b <= 9)
+		{
+			prod = a * b;
+			
+			_putchar(prod >= 10 ? (prod/10)%10 + '0' : ' ');
+			_putchar(prod%10 + '0');
+			if (b != 9)
+				_putchar(',');
+			_putchar(' ');
+			b++;
+			
+		}
+		_putchar('\n');
+		a++;
+	}
+}
