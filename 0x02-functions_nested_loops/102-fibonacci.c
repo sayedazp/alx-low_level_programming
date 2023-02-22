90deb3157077e6fad10d9a944070b2e5240f2ab0
sayedazp <sayedazp@gmail.com>
2023-02-21 18:37:39 -0800

    task14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,19 +6,19 @@
 
 int main(void)
 {
-	long prev = 1, prevv = 0 , c = 0 , current;
+	long prev = 1, prevv = 0, c = 0, current;
 
 	while (c  < 50)
 	{
 		current = prev + prevv;
 		prevv = prev;
 		prev = current;
-		c++;
-		
-		if (c < 49 )
+
+		if (c < 49)
 			printf("%ld, ", current);
 		else
 			printf("%ld", current);
+		c++;
 	}
 	printf("\n");
 	return (0);
