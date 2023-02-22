31e42e9904ca201e6a2f20a2262c29e21a57526e
sayedazp <sayedazp@gmail.com>
2023-02-21 18:16:18 -0800

    task13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/***/
+int main(void)
+{
+	int c = 0;
+	int sum = 0;
+
+	while (c < 1024)
+	{
+		if (c % 3 == 0 || c % 5 == 0)
+		{
+			sum += c;
+		}
+		c++;
+
+	}
+	printf("%d", sum);
+	return (0);
+}
