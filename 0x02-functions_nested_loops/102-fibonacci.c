4dacaeb14d6304ba0751f57b18811e9a4ca0108e
sayedazp <sayedazp@gmail.com>
2023-02-21 18:31:16 -0800

    task14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-/***/
+/**
+ * mian - entry point func
+ * Return: always 0
+*/
 
 int main(void)
 {
-	int prev = 1, prevv = 0 , c = 0 , current;
+	long long prev = 1, prevv = 0 , c = 0 , current;
 
-	while (c  < 98)
+	while (c  < 50)
 	{
 		current = prev + prevv;
 		prevv = prev;
