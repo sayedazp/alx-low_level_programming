258a91e337e5e6a5b0dafb4e40b5c69e4bd55f6e
sayedazp <sayedazp@gmail.com>
2023-02-23 11:08:23 -0800

    task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - print line with length n
+ * @n: the length
+*/
+
+void print_numbers(int n)
+{
+	while (n--)
+	{
+		if (n <= 0)
+			break;
+		_putchar('_');
+	}
+	_putchar('\n');
+}
