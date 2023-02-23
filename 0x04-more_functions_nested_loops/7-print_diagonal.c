72cffaaa7b61265181eb8160cf300b381b495b32
sayedazp <sayedazp@gmail.com>
2023-02-23 14:35:45 -0800

    task7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,18 +8,21 @@ void print_diagonal(int n)
 	int c, x;
 
 	c = x = n;
-	if (n <= 0)
-		_putchar('\n');
 	while (n--)
 	{
 		c = x;
 
+		if (n < 0)
+		{
+			_putchar('\n');
+			break;
+		}
 		while (c > n + 1)
 		{
 			_putchar(' ');
 			c--;
 		}
-		_putchar('\');
+		_putchar('\\');
 		_putchar('\n');
 	}
 }
