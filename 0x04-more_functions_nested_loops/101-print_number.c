ddababbeded85e6521ae01c335e26c317159898c
sayedazp <sayedazp@gmail.com>
2023-02-23 17:22:07 -0800

    task12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,12 +5,15 @@
 */
 void print_number(int n)
 {
+	unsigned int num;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
 	}
-	if (n / 10 > 0)
-		print_number(n / 10);
-	_putchar(n % 10 + '0');
+	num = n;
+	if (num / 10 > 0)
+		print_number(num / 10);
+	_putchar(num % 10 + '0');
 }
