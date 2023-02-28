b39f410802c71515c1111f1b093f1fb712d57974
sayedazp <sayedazp@gmail.com>
2023-02-27 17:38:19 -0800

    task10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+/**
+ * _atoi - some crazy func
+ * @s: the desired text
+ * Return: the output int
+*/
+int _atoi(char *s)
+{
+	int i = 0;
+	int n = 0;
+	int p = 1;
+	int sign = 1;
+	int c = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			c++;
+			if (s[i - 1] == '-')
+				sign = -1;
+			if (!(s[i + 1] >= 48 && s[i + 1] <= 57))
+				break;
+		}
+		i++;
+	}
+	while (c--)
+	{
+		n += p * (s[i--] - '0');
+		p *= 10;
+	}
+	return (n * sign);
+}
