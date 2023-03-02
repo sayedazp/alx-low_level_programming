d5d62012a74e9f492bdd57aa59789404b9ce7f39
sayedazp <sayedazp@gmail.com>
2023-03-02 12:59:47 -0800

    task1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,11 +9,18 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int d = 0;
+	int i = 0;
 
 	while (d < n)
 	{
+		if (dest[d] == '\0')
+			i = 1;
 		dest[d] = src[d];
 		d++;
+	}
+	if (i == 1)
+	{
+		dest[d] = '\0';
 	}
 	return (dest);
 
