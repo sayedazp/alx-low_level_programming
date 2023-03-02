4258848647e93ac8169e3a56d78ffd1c310aaf84
sayedazp <sayedazp@gmail.com>
2023-03-02 15:20:53 -0800

    task6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+/**
+ * cap_string - function to capitalize letters
+ * @a: the input array
+ * Return: Return char array
+*/
+char *cap_string(char *a)
+{
+	int i = 0;
+	int lis[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int l = 0;
+
+	while (a[i] != '\0')
+	{
+		while (l < sizeof(lis) / sizeof(int) && (a[i] >= 'a' && a[i] <= 'z'))
+		{
+			if (a[i - 1] == lis[l])
+			{
+				a[i] -= 32;
+			}
+			l++;
+		}
+		l = 0;
+		i++;
+	}
+	return (a);
+}
