e0e6237f9432dede1c73d5e92d132c87de102810
sayedazp <sayedazp@gmail.com>
2023-03-06 14:58:44 -0800

    task1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+/**
+ * _memcpy - copy elements from src to dest
+ * @dest: the desired destination
+ * @src: the source to cpy from
+ * @n: number of compied bytes
+ * Return: returns a char pointer
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] == src[i];
+	}
+	return (dest);
+}
