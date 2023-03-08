88d3d1104f3fd37667acd116bcb96e047f6a8a69
sayedazp <sayedazp@gmail.com>
2023-03-07 22:50:18 -0800

    task4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "mian.h"
+/**
+ * _pow_recursion - a function to compute numbers raised to power
+ * @x: first inp
+ * @y: second inp
+ * Return: the returned value
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	return (x * _pow_recursion(x, y - 1));
+}
