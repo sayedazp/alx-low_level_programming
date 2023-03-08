d9e35698a1c5945ec06f4975ac39566ada5d0528
sayedazp <sayedazp@gmail.com>
2023-03-07 22:42:13 -0800

    task3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "mian.h"
+/**
+ * factorial - computes factorial
+ * @n: the target num
+ * Return: the desired factorial
+*/
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	return (n * factorial(n - 1));
+}
