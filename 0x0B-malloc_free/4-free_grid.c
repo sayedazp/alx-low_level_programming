4c58b3371a203cd394c981403595f2c885521be0
sayedazp <sayedazp@gmail.com>
2023-03-14 15:57:39 -0700

    task4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - a func to cncat 2 strs
+ * @x: first str to concate
+ * @height: second str to concate
+*/
+void free_grid(int **x, int height)
+{
+	int **x;
+	int i = 0;
+
+	while (i < height)
+	{
+		if (*(x + i) == NULL)
+		{
+			for (i = 0; i < height; i++)
+				free(x[i]);
+			free(x);
+		}
+		i++;
+	}
+}
