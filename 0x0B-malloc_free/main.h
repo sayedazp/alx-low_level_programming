122ca24faa0817b808a01d7569791844ffa6bd91
sayedazp <sayedazp@gmail.com>
2023-03-14 16:38:23 -0700

    task5

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,6 +3,7 @@
 
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
