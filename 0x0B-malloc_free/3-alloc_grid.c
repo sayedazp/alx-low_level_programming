f8571e874e728adf0e1c23a78a93f8b391606790
sayedazp <sayedazp@gmail.com>
2023-03-14 15:32:48 -0700

    task3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - a func to cncat 2 strs
+ * @width: first str to concate
+ * @height: second str to concate
+ * Return: the concated one
+*/
+int **alloc_grid(int width, int height)
+{
+	int **x;
+	int i = 0;
+	int z = 0;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	x = (int **)(malloc(sizeof(int *) * height));
+	if (x == NULL)
+		return (NULL);
+	while (i < width)
+	{
+		*(x + i) = (int *)malloc(sizeof(int) * width);
+		if (*(x + i) == NULL)
+			return (NULL);
+		i++;
+	}
+	i = 0;
+	while (i < height)
+	{
+		while (z < width)
+		{
+			x[height][width] = 0;
+			z++;
+		}
+		i++;
+	}
+	return (x);
+}
