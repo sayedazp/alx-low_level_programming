8e9476666a4ed34e96b4a0beeed9cc1220d54685
sayedazp <sayedazp@gmail.com>
2023-03-16 07:32:12 -0700

    task0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -12,16 +12,10 @@ void *malloc_checked(unsigned int b)
 	void *x;
 
 	if (b <= 0)
-	{
 		exit(98);
-		return (NULL);
-	}
 	x = malloc(b);
 	if (x == NULL)
-	{
 		exit(98);
-		return (NULL);
-	}
 	return (x);
 
 }
