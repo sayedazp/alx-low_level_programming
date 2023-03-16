adc360c19c89d343ebbe655315dd266106556d62
sayedazp <sayedazp@gmail.com>
2023-03-16 07:23:05 -0700

    task0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - a func
+ * @b: a size to allocate
+ *Return: void pointer to the allocated space
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *x;
+
+	if (b <= 0)
+		return (NULL);
+	x = malloc(b);
+	if (x == NULL)
+		exit(98);
+	return (x);
+
+}
