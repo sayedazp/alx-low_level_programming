80e7bda6ad162e017ffe40534b529eb76b4ea2ec
sayedazp <sayedazp@gmail.com>
2023-03-23 16:58:26 -0700

    task0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,19 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * add - a func to add
+ * @count: a needed param
+ * Return: an integer
+*/
+int add(const int count, ...)
+{
+	va_list ap;
+	int i, sum;
+
+	va_start(ap, count);
+	sum = 0;
+	for (i = 0; i < count; i++)
+		sum += va_arg(ap, int);
+	va_end(ap);
+	return (sum);
+}
