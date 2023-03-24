8fdf29840a0799db46c35768fa31e341b5ba5427
sayedazp <sayedazp@gmail.com>
2023-03-23 17:40:16 -0700

    task2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_strings - print variadic number
+ * @separator: char
+ * @n:variadic variable count
+ * Return :void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		if (separator && i != n - 1)
+			printf("%s%s", va_arg(ap, char*), separator);
+		else
+			printf("%s", va_arg(ap, char*));
+	}
+	printf("\n");
+	va_end(ap);
+}
