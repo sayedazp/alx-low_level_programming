5030a230eeaade234406527276f7312675d8c0e2
sayed azp <sayedazp@gmail.com>
2023-06-16 15:04:11 +0300

    task5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - a func to get a node at ll index
+ * @head: a pointer pointer to the head of the linked list
+ * @index: a number for node to hold
+ * Return: poointer of newly added node
+ * **/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	int i = 0;
+	dlistint_t *tmp = head;
+
+	if (!head)
+		return (NULL);
+
+	while (tmp && i <= index)
+	{
+		if (i == index)
+			return (tmp);
+		i++;
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
