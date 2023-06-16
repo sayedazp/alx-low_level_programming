12ffe7a6e058ad8508f574c49a2d30896f31cc5e
sayed azp <sayedazp@gmail.com>
2023-06-16 17:01:05 +0300

    task7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,39 +1,46 @@
 #include "lists.h"
+
 /**
- * insert_dnodeint_at_index - a func to insert a node at ll index
- * @h: a pointer pointer to the head of the linked list
- * @idx: index
- * @n: num to add
- * Return: newly added or null
- * **/
+ * insert_dnodeint_at_index - function with three arguments
+ * @h: pointer to doubly linked list
+ * @idx: index position to insert node
+ * @n: value of new node
+ *
+ * Description: inserting new node into a doubly linked list
+ * Return: address of new node or NULL if failed
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *iter = *h, *new;
-	unsigned int i = 0;
+	dlistint_t *new, *cursor;
 
-	if (!h)
+	if (h == NULL)
 		return (NULL);
-
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	while (iter)
+	/* loop until nth node of idx */
+	cursor = *h;
+	while (idx > 1 && cursor && cursor->next)
 	{
-		if (i == idx)
-		{
-			new = malloc(sizeof(dlistint_t));
-			if (!new)
-				return (NULL);
-			new->next = iter;
-			new->n = n;
-			iter->prev->next = new;
-			new->prev = iter->prev;
-			if (iter != NULL)
-				iter->prev = new;
-			return (new);
-		}
-		iter = iter->next;
-		i++;
+		cursor = cursor->next;
+		idx--;
 	}
-	return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	if (idx > 1 || cursor == NULL)
+		return (NULL);
+
+	/* assign value to new node */
+	new->n = n;
+
+	/* insert node */
+	if (cursor->next != NULL)
+		cursor->next->prev = new;
+	new->prev = cursor;
+	new->next = cursor->next;
+	cursor->next = new;
+
+	return (new);
 }
