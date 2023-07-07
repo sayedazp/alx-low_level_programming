296046c4eb5ce02ea16b98ad75c3e021fbf0057a
sayed azp <sayedazp@gmail.com>
2023-07-08 02:14:49 +0300

    task4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,53 +1,100 @@
-# include "hash_tables.h"
+#include "hash_tables.h"
 
 /**
- * hash_table_set - function that adds an element to the hash table
- * @ht: pointer to hash table
- * @key: key to add the element
- * @value: value to add the element
+ * hash_table_set - insert a key, value pair to a hash table.
+ * @ht: hash table to insert to.
+ * @key: key
+ * @value: value to be stored at that key.
  *
- * Return: 1 if it succeeded, 0 otherwise
+ * Return: 1 on SUCCESS otherwise 0
  */
-
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = 0;
-	char *valuecopy, *keycopy;
-	hash_node_t  *bucket, *new_node;
+	unsigned long int idx;
+	char *str;
+	hash_node_t *node;
 
-	if (!ht || !key || !*key || !value)
+	if (!ht || !key)
 		return (0);
 
-	valuecopy = strdup(value);
-	if (!valuecopy)
-		return (0);
+	idx = key_index((unsigned char *)key, ht->size);
+	if (value)
+		str = strdup(value);
+	else
+		str = NULL;
 
-	index = key_index((const unsigned char *)key, ht->size);
-	bucket = ht->array[index];
+	node = ht->array[idx];
 
-	while (bucket)
+	if (!node)
+		node = new_node(NULL);
+
+	if (node->key)
 	{
-		if (!strcmp(key, bucket->key))
-		{
-			free(bucket->value);
-			bucket->value = valuecopy;
-			return (1);
-		}
-		bucket = bucket->next;
+		ht->array[idx] = update_node(node, key, str);
+		return (1);
 	}
-	new_node = calloc(1, sizeof(hash_node_t));
-	if (new_node == NULL)
-	{
-		free(valuecopy);
-		return (0);
-	}
-	keycopy = strdup(key);
-	if (!keycopy)
-		return (0);
-	new_node->key = keycopy;
-	new_node->value = valuecopy;
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+	node->key = strdup(key);
+	node->value = str;
+	node->next = NULL;
+
+	ht->array[idx] = node;
 	return (1);
+}
+
+/**
+ * update_node - update or added a node if one already exists
+ *
+ * @node: pointer to a node the exist
+ * @key: key
+ * @value: value to update/create node with.
+ *
+ * Return: pointer to head of a list
+ */
+hash_node_t *update_node(hash_node_t *node, const char *key, char *value)
+{
+	hash_node_t *tmp = node;
+
+	while (tmp)
+	{
+
+		if (!strcmp(key, tmp->key))
+		{
+			free(tmp->value);
+			tmp->value = value;
+			return (node);
+		}
+		tmp = tmp->next;
+	}
+
+	tmp = node;
+	node = new_node(node);
+	if (!node)
+	{
+		node = tmp;
+		return (node);
+	}
+	node->key = strdup(key);
+	node->value = value;
+	return (node);
+
+}
+
+/**
+ * new_node - create a new node
+ * @c_node: current node to add add at the end.
+ *
+ * Return: a point to the new node.
+ */
+hash_node_t *new_node(hash_node_t *c_node)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (NULL);
+	node->key = NULL;
+	node->value = NULL;
+	node->next = c_node;
+
+	return (node);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -36,5 +36,6 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-
+hash_node_t *update_node(hash_node_t *node, const char *key, char *value);
+hash_node_t *new_node(hash_node_t *c_node);
 #endif /*_HASH_TABLES_H_*/
