8664a25a5fa3bef5de866935c27f0ece13f33013
sayed azp <sayedazp@gmail.com>
2023-07-08 02:11:50 +0300

    task4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,7 +36,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		bucket = bucket->next;
 	}
-	new_node = malloc(sizeof(hash_node_t));
+	new_node = calloc(1, sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
 		free(valuecopy);
