7f4736d0de390d33ceedc74a0e49f787930d2d7e
sayed azp <sayedazp@gmail.com>
2023-07-08 01:45:51 +0300

    init

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -12,9 +12,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	 char *key;
+	 char *value;
+	 struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -27,7 +27,7 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	 unsigned long int size;
+	 hash_node_t **array;
 } hash_table_t;
 #endif /*_HASH_TABLES_H_*/
